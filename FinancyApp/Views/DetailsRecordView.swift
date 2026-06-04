3bd4c148d3be6a3f3610dbfe4464e962e37cd29e
alexgi <060agiribar2017@gmail.com>
2026-06-03 21:37:07 -0400

    first commit

---
+++ FinancyApp/Views/DetailsRecordView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsRecordView.swift
+//  FinancyApp
+//
+//  Created by AlexGI on 03/06/2026.
+//
+
+import SwiftUI
+
+struct DetailsRecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsRecordView()
+}
